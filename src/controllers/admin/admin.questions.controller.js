76a8bc37aad0da9a94f70319c4ebd97df6261e30
Michael Toth <mtotho@gmail.com>
2015-04-08 08:02:37 -0400

    rough question set ui. can create stubbed questions and sets

--- src/controllers/admin/admin.questions.controller.js
+++ src/controllers/admin/admin.questions.controller.js
@@ -5,24 +5,55 @@ angular.module('placemapApp')
         var vm=this;
 
         var QuestionSet = $resource('/api/v1/questionsets');
+        var Question = $resource('/api/v1/questions');
 
-
-        vm.questionsets=[{name:"one"},{name:"two"}];
-
+        vm.questionsets=[];
+        vm.questions=[];
         vm.newQS = {
             name:""
         };
+        vm.newQuestion  = new Question();
+
+        QuestionSet.query(function(data){
+            vm.questionsets = data;
+        });
+        Question.query(function(data){
+            vm.questions = data;
+        });
+
+
         $scope.createSet = function(form){
 
             if(form.$valid) {
                 var qs = new QuestionSet(vm.newQS);
-             
+
                 qs.$save(function(data){
                     console.log(data);
                 });
-
-
             }
+        }
+
+        $scope.createQuestion = function(form){
+            if(form.$valid) {
+                var q= new Question(vm.newQuestion);
+
+                q.$save(function(data){
+                    console.log(data);
+
+                    vm.questions.push(data);
+                    vm.newQuestion = new Question();
+                });
+            }
+
+        }
+
+        function Question(){
+            this.name = "";
+            this.text = "";
+            this.type = "shortanswer";
+            this.tags = [];
+            this.opts = [];
+
         }
         //vm.places = null;
         //
